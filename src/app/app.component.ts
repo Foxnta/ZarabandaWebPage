47675fee7f03dda4c84b21a8c9fc03b8f94c2e8b
Foxnta <nicolastovar9@gmail.com>
2024-08-08 15:09:13 -0500

    rueda giratoria

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeroComponent } from './hero/hero.component';
+import { MainComponent } from './main/main.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, HeroComponent, FooterComponent],
+  imports: [RouterOutlet, NavbarComponent, HeroComponent, MainComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
