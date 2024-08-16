6a3f6d2a4a35a231505354e3b10365cfaee3d699
Foxnta <nicolastovar9@gmail.com>
2024-08-16 13:37:44 -0500

    correccion pdf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,23 +5,34 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeroComponent } from './hero/hero.component';
 import { MainComponent } from './main/main.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, HeroComponent, MainComponent, FooterComponent],
+  imports: [RouterOutlet, NavbarComponent, HeroComponent, MainComponent, FooterComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'zarabanda';
-  constructor(private router: Router) {}
+  constructor(private router: Router) { }
+
+  showFooter: boolean = true;
 
   ngOnInit() {
     this.router.events.subscribe(event => {
       if (event instanceof NavigationEnd) {
+
         window.scrollTo(0, 0);
       }
+      if (this.router.url.includes('projectview')) {
+        this.showFooter = false;
+      }else
+      {
+        this.showFooter = true;
+      }
     });
-  }
+
+  };
 }
